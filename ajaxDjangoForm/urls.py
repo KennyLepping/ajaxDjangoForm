3376de13f84c9341272c294d69b298a3696ebeda
Kenny Lepping <kennylepping@gmail.com>
2021-02-10 01:05:58 -0500

    FBV AJAX User details from DB into JSON

--- ajaxDjangoForm/urls.py
+++ ajaxDjangoForm/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 # from core import views as core
+from core2 import views as core2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+
+    #app_2
+    path('user', core2.userPanel),
+    path('ajax/get_user_info', core2.getUserInfo, name = 'get_user_info'),
 ]
